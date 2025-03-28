eb8e6ba414a923220f3923e8f8f1516c5ee7e30d
Simon Brunning <simon.brunning3@nhs.net>
2025-03-28 12:33:42 +0000

    Quieten one more 3rd party library.

--- src/eligibility_signposting_api/config.py
+++ src/eligibility_signposting_api/config.py
@@ -25,7 +25,7 @@ def config() -> dict[str, Any]:
     }
 
 
-def init_logging(quieten: Sequence[str] = ("asyncio", "botocore", "boto3", "mangum")) -> None:
+def init_logging(quieten: Sequence[str] = ("asyncio", "botocore", "boto3", "mangum", "urllib3")) -> None:
     log_format = "%(asctime)s %(levelname)-8s %(name)s %(module)s.py:%(funcName)s():%(lineno)d %(message)s"
     formatter = JsonFormatter(log_format)
     handler = logging.StreamHandler()
