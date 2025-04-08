363fa8607534120350edd81392c488c0dce2eb31
Simon Brunning <simon.brunning3@nhs.net>
2025-04-08 11:25:44 +0100

    Try latest version?

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -9,4 +9,4 @@ if ! [ -x "$(command -v poetry)" ]; then
   fi
   pipx install poetry
 fi
-poetry self add poetry-plugin-lambda-build@1.0.8 poetry-plugin-export@1.9.0
+poetry self add poetry-plugin-lambda-build@2.1.0 poetry-plugin-export@1.9.0
