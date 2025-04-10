9dd6e711887444e8d503a02c04736028a28ee05c
Edd Almond <edd.almond1@nhs.net>
2025-04-10 16:35:48 +0100

    eli-201 commenting out SSM as we're not at the point where we have certificates for domains

--- infrastructure/stacks/networking/variables.tf
+++ infrastructure/stacks/networking/variables.tf
@@ -1,24 +1,22 @@
-variable "PROXYGEN_PRIVATE_KEY" {
-  type        = string
-  description = "The private key for proxygen"
-  sensitive   = true
-  default     = "Only available on DEV"
-}
-
-variable "API_CA_CERT" {
-  type        = string
-  description = "The Certificate Authority (CA) Root for the Client Certificate with Intermediate Certificate"
-  sensitive   = true
-}
-
-variable "API_CLIENT_CERT" {
-  type        = string
-  description = "The signed Client Certificate"
-  sensitive   = true
-}
-
-variable "API_PRIVATE_KEY_CERT" {
-  type        = string
-  description = "The private key for the signed Client Certificate"
-  sensitive   = true
-}
+# variable "API_CA_CERT" {
+#   type        = string
+#   description = "The Certificate Authority (CA) Root for the Client Certificate with Intermediate Certificate"
+#   sensitive   = true
+#   default     = "Test Value"
+#
+# }
+#
+# variable "API_CLIENT_CERT" {
+#   type        = string
+#   description = "The signed Client Certificate"
+#   sensitive   = true
+#   default     = "Test Value"
+# }
+#
+# variable "API_PRIVATE_KEY_CERT" {
+#   type        = string
+#   description = "The private key for the signed Client Certificate"
+#   sensitive   = true
+#   default     = "Test Value"
+# }
+#
