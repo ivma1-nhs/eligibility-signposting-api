c760532b8aa28e512f5f16b7a4861853db0a93d6
Simon Brunning <simon.brunning3@nhs.net>
2025-05-19 11:18:55 +0100

    Make sure passed None's are preserved in person_rows_builder().

--- tests/fixtures/builders/repos/person.py
+++ tests/fixtures/builders/repos/person.py
@@ -43,39 +43,39 @@ class PersonDetailProvider(BaseProvider):
 def person_rows_builder(  # noqa:PLR0913
     nhs_number: str,
     *,
-    date_of_birth: date | None = None,
-    gender: Literal["0", "1"] | None = None,
-    postcode: str | None = None,
-    cohorts: Sequence[str] | None = None,
-    vaccines: Sequence[str] | None = None,
-    icb: str | None = None,
-    gp_practice: str | None = None,
-    pcn: str | None = None,
-    comissioning_region: str | None = None,
-    thirteen_q: bool | None = None,
-    care_home: bool | None = None,
-    de: bool | None = None,
-    msoa: str | None = None,
-    lsoa: str | None = None,
+    date_of_birth: date | None = ...,
+    gender: Literal["0", "1"] | None = ...,
+    postcode: str | None = ...,
+    cohorts: Sequence[str] | None = ...,
+    vaccines: Sequence[str] | None = ...,
+    icb: str | None = ...,
+    gp_practice: str | None = ...,
+    pcn: str | None = ...,
+    comissioning_region: str | None = ...,
+    thirteen_q: bool | None = ...,
+    care_home: bool | None = ...,
+    de: bool | None = ...,
+    msoa: str | None = ...,
+    lsoa: str | None = ...,
 ) -> list[dict[str, Any]]:
     faker = Faker("en_UK")
     faker.add_provider(PersonDetailProvider)
 
     key = f"PERSON#{nhs_number}"
-    date_of_birth = date_of_birth or faker.date_of_birth(minimum_age=18, maximum_age=99)
-    gender = gender or choice(("0", "1"))
-    postcode = postcode or faker.postcode()
-    cohorts = cohorts if cohorts is not None else ["cohort-a", "cohort-b"]
-    vaccines = vaccines if vaccines is not None else ["RSV", "COVID"]
-    icb = icb or faker.icb()
-    gp_practice = gp_practice or faker.gp_practice()
-    pcn = pcn or faker.pcn()
-    comissioning_region = comissioning_region or faker.comissioning_region()
-    thirteen_q = thirteen_q if thirteen_q is not None else faker.boolean()
-    care_home = care_home if care_home is not None else faker.boolean()
-    de = de if de is not None else faker.boolean()
-    msoa = msoa or faker.msoa()
-    lsoa = lsoa or faker.lsoa()
+    date_of_birth = date_of_birth if date_of_birth is not ... else faker.date_of_birth(minimum_age=18, maximum_age=99)
+    gender = gender if gender is not ... else choice(("0", "1"))
+    postcode = postcode if postcode is not ... else faker.postcode()
+    cohorts = cohorts if cohorts is not ... else ["cohort-a", "cohort-b"]
+    vaccines = vaccines if vaccines is not ... else ["RSV", "COVID"]
+    icb = icb if icb is not ... else faker.icb()
+    gp_practice = gp_practice if gp_practice is not ... else faker.gp_practice()
+    pcn = pcn if pcn is not ... else faker.pcn()
+    comissioning_region = comissioning_region if comissioning_region is not ... else faker.comissioning_region()
+    thirteen_q = thirteen_q if thirteen_q is not ... else faker.boolean()
+    care_home = care_home if care_home is not ... else faker.boolean()
+    de = de if de is not ... else faker.boolean()
+    msoa = msoa if msoa is not ... else faker.msoa()
+    lsoa = lsoa if lsoa is not ... else faker.lsoa()
     rows: list[dict[str, Any]] = [
         {
             "NHS_NUMBER": key,
