651f05056c0edefa9565e3b692691716ff99beee
Simon Brunning <simon.brunning3@nhs.net>
2025-05-19 12:21:20 +0100

    1st things 1st.

--- tests/fixtures/builders/repos/person.py
+++ tests/fixtures/builders/repos/person.py
@@ -10,38 +10,6 @@ from faker.providers import BaseProvider
 Gender = Literal["0", "1"]
 
 
-class PersonDetailProvider(BaseProvider):
-    def icb(self) -> str | None:
-        if randint(0, 3):
-            return f"{choice(string.ascii_uppercase)}{choice(string.ascii_uppercase)}{choice(string.digits)}"
-        return None
-
-    def gp_practice(self) -> str | None:
-        if randint(0, 3):
-            return f"{choice(string.ascii_uppercase)}{randint(1, 99999):05}"
-        return None
-
-    def pcn(self) -> str | None:
-        if randint(0, 3):
-            return f"{choice(string.ascii_uppercase)}{randint(1, 99999):05}"
-        return None
-
-    def comissioning_region(self) -> str | None:
-        if randint(0, 3):
-            return f"{choice(string.ascii_uppercase)}{randint(1, 99):02}"
-        return None
-
-    def msoa(self) -> str | None:
-        if randint(0, 3):
-            return f"{choice(string.ascii_uppercase)}{randint(1, 99999999):08}"
-        return None
-
-    def lsoa(self) -> str | None:
-        if randint(0, 3):
-            return f"{choice(string.ascii_uppercase)}{randint(1, 99999999):08}"
-        return None
-
-
 def person_rows_builder(  # noqa:PLR0913
     nhs_number: str,
     *,
@@ -120,3 +88,35 @@ def person_rows_builder(  # noqa:PLR0913
 
     shuffle(rows)
     return rows
+
+
+class PersonDetailProvider(BaseProvider):
+    def icb(self) -> str | None:
+        if randint(0, 3):
+            return f"{choice(string.ascii_uppercase)}{choice(string.ascii_uppercase)}{choice(string.digits)}"
+        return None
+
+    def gp_practice(self) -> str | None:
+        if randint(0, 3):
+            return f"{choice(string.ascii_uppercase)}{randint(1, 99999):05}"
+        return None
+
+    def pcn(self) -> str | None:
+        if randint(0, 3):
+            return f"{choice(string.ascii_uppercase)}{randint(1, 99999):05}"
+        return None
+
+    def comissioning_region(self) -> str | None:
+        if randint(0, 3):
+            return f"{choice(string.ascii_uppercase)}{randint(1, 99):02}"
+        return None
+
+    def msoa(self) -> str | None:
+        if randint(0, 3):
+            return f"{choice(string.ascii_uppercase)}{randint(1, 99999999):08}"
+        return None
+
+    def lsoa(self) -> str | None:
+        if randint(0, 3):
+            return f"{choice(string.ascii_uppercase)}{randint(1, 99999999):08}"
+        return None
