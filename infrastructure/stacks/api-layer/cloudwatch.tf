a2dfe4fd05791b821ea264ce511e4a8f30e6abb9
Edd Almond <edd.almond1@nhs.net>
2025-06-02 15:58:58 +0100

    eli-238 CKV_AWS_158 - adding KMS encryption using lambda cmk

--- infrastructure/stacks/api-layer/cloudwatch.tf
+++ infrastructure/stacks/api-layer/cloudwatch.tf
@@ -2,6 +2,7 @@
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   name              = "/aws/lambda/${module.eligibility_signposting_lambda_function.aws_lambda_function_id}"
   retention_in_days = 365
+  kms_key_id        = module.eligibility_signposting_api_gateway.lambda_cmk.arn
 
   tags = {
     Name  = "lambda-execution-logs"
