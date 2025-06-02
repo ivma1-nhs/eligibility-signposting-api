fc945ae5d00ad53d1ffb35628b219c60e2875fe2
Edd Almond <edd.almond1@nhs.net>
2025-06-02 15:22:45 +0100

    eli-238 CKV_AWS_115: Concurrent execution limit will be set at APIM level, not at Lambda leve

--- infrastructure/modules/lambda/lambda.tf
+++ infrastructure/modules/lambda/lambda.tf
@@ -1,5 +1,6 @@
 resource "aws_lambda_function" "eligibility_signposting_lambda" {
   #checkov:skip=CKV_AWS_116: No deadletter queue is configured for this Lambda function, yet
+  #checkov:skip=CKV_AWS_115: Concurrent execution limit will be set at APIM level, not at Lambda level
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = var.file_name
