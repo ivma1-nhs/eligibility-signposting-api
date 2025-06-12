36cb4f9454da2929fc5f0b6ae7f811be2eae3196
Edd Almond <edd.almond1@nhs.net>
2025-06-12 16:15:49 +0100

    making acm imported cert take into account count on ssm params

--- infrastructure/stacks/networking/acm_certificates.tf
+++ infrastructure/stacks/networking/acm_certificates.tf
@@ -1,15 +1,15 @@
 resource "aws_acm_certificate" "imported_cert" {
-  private_key       = aws_ssm_parameter.mtls_api_private_key_cert.value
-  certificate_body  = aws_ssm_parameter.mtls_api_client_cert.value
-  certificate_chain = aws_ssm_parameter.mtls_api_ca_cert.value
+  private_key       = can(data.aws_ssm_parameter.existing_private_key_cert.id) ? data.aws_ssm_parameter.existing_private_key_cert.value : aws_ssm_parameter.mtls_api_private_key_cert[0].value
+  certificate_body  = can(data.aws_ssm_parameter.existing_client_cert.id) ? data.aws_ssm_parameter.existing_client_cert.value : aws_ssm_parameter.mtls_api_client_cert[0].value
+  certificate_chain = can(data.aws_ssm_parameter.existing_ca_cert.id) ? data.aws_ssm_parameter.existing_ca_cert.value : aws_ssm_parameter.mtls_api_ca_cert[0].value
 
   lifecycle {
     create_before_destroy = true
   }
 
   tags = {
-    Region        = local.region
-    Stack         = local.stack_name
+    Region          = local.region
+    Stack           = local.stack_name
     CertificateType = "Imported"
   }
 }
